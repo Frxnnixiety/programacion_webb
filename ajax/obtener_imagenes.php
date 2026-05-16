bc7730a586418b8b48ff1bab9ff75bc9ae07badd
Frxnnixiety <franceliaelguera@gmail.com>
2026-05-15 21:18:30 -0600

    cambio de ruta

--- ajax/obtener_imagenes.php
+++ ajax/obtener_imagenes.php
@@ -1,16 +1,27 @@
 <?php
+
 include("../config/conexion.php");
+// Inicializar la conexión PDO
 $conexion = conectarDB();
 
 $sql = "SELECT * FROM imagenes ORDER BY id DESC";
+
+// Ejecutamos la consulta
 $resultado = $conexion->query($sql);
 
 $imagenes = [];
+
 while($fila = $resultado->fetch()){
-    // Reemplaza los "../" iniciales para que la ruta sea relativa a la raíz del sitio
-    $fila['ruta'] = str_replace("../", "", $fila['ruta']);
+
+    // SOLUCIÓN: Limpiamos el "../" inicial para que la ruta sea relativa correcta en Linux
+    // Si la ruta es "../uploads/imagen.png", pasará a ser "uploads/imagen.png"
+    if (substr($fila['ruta'], 0, 3) === '../') {
+        $fila['ruta'] = substr($fila['ruta'], 3);
+    }
+
     $imagenes[] = $fila;
 }
 
 echo json_encode($imagenes);
+
 ?>
